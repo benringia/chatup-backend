5b36a6acf863ef8115a9fa61a6012486c9687128
Ben Clauser Ringia <benringia@gmail.com>
2020-07-01 11:21:55 +0800

    Set up schema, routes for chat messages

--- server.js
+++ server.js
@@ -59,14 +59,14 @@ const io = require('socket.io').listen(server);
 // const { User } = require('./Helpers/UserClass');
 
 require('./socket/streams')(io);
-// require('./socket/private')(io);
+require('./socket/private')(io);
 
 const dbConfig = require('./config/secret');
 const auth = require('./routes/authRoutes');
 const posts = require('./routes/postRoutes');
 const users = require('./routes/userRoutes');
 const friends = require('./routes/friendsRoutes');
-// const message = require('./routes/messageRoutes');
+const message = require('./routes/messageRoutes');
 // const image = require('./routes/imageRoutes');
 
 app.use(express.json({ limit: '50mb' }));
@@ -84,7 +84,7 @@ app.use('/api/chatup', auth);
 app.use('/api/chatup', posts);
 app.use('/api/chatup', users);
 app.use('/api/chatup', friends);
-// app.use('/api/chatapp', message);
+app.use('/api/chatup', message);
 // app.use('/api/chatapp', image);
 
 server.listen(3000, () => {
