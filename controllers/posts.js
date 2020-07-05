5579da3bb345c3d44267875abbb2801bf1a4ed48
Ben Clauser Ringia <benringia@gmail.com>
2020-07-05 17:22:55 +0800

    Added post image func, added Live location on users and posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,55 +1,116 @@
 const Joi = require('joi');
 const HttpStatus = require('http-status-codes');
+const cloudinary = require('cloudinary');
+const moment = require('moment');
+const request = require('request');
+
+cloudinary.config({
+  cloud_name: 'dpcl2xifo',
+  api_key: '385413283214345',
+  api_secret: 'xYrbZz8TrCjPrDsnJEYjJKtEPuY'
+});
 
 const Post = require('../models/postModels');
 const User = require('../models/userModels');
 
 module.exports = {
-    AddPost(req, res) {
-       const schema = Joi.object().keys({
-           post: Joi.string().required()
-       });
-       const { error } = Joi.validate(req.body, schema);
-       if(error && error.details){
-        return res.status(HttpStatus.BAD_REQUEST).json({ msg: error.details });
-       }
-       const body = {
-           user: req.user._id,
-           username: req.user.username,
-           post: req.body.post,
-           created: new Date()
-       }
-       Post.create(body).then(async (post) => {
-            await User.update({
+  AddPost(req, res) {
+    const schema = Joi.object().keys({
+      post: Joi.string().required()
+    });
+    const body = {
+      post: req.body.post
+    };
+    const { error } = Joi.validate(body, schema);
+    if (error && error.details) {
+      return res.status(HttpStatus.BAD_REQUEST).json({ msg: error.details });
+    }
+    const bodyObj = {
+      user: req.user._id,
+      username: req.user.username,
+      post: req.body.post,
+      created: new Date()
+    };
+
+    if (req.body.post && !req.body.image) {
+      Post.create(bodyObj)
+        .then(async post => {
+          await User.update(
+            {
+              _id: req.user._id
+            },
+            {
+              $push: {
+                posts: {
+                  postId: post._id,
+                  post: req.body.post,
+                  created: new Date()
+                }
+              }
+            }
+          );
+          res.status(HttpStatus.OK).json({ message: 'Post created', post });
+        })
+        .catch(err => {
+          res
+            .status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .json({ message: 'Error occured' });
+        });
+    }
+
+    if (req.body.post && req.body.image) {
+      cloudinary.uploader.upload(req.body.image, async result => {
+        const reqBody = {
+          user: req.user._id,
+          username: req.user.username,
+          post: req.body.post,
+          imgId: result.public_id,
+          imgVersion: result.version,
+          created: new Date()
+        };
+        Post.create(reqBody)
+          .then(async post => {
+            await User.update(
+              {
                 _id: req.user._id
-            },{
+              },
+              {
                 $push: {
                   posts: {
                     postId: post._id,
                     post: req.body.post,
                     created: new Date()
+                  }
                 }
-            }
-            }
+              }
             );
-           res.status(HttpStatus.OK).json({message: 'Post created', post});
-       })
-       .catch(err => {
-           res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({message: 'Error occured'})
-       });
-   },
+            res.status(HttpStatus.OK).json({ message: 'Post created', post });
+          })
+          .catch(err => {
+            res
+              .status(HttpStatus.INTERNAL_SERVER_ERROR)
+              .json({ message: 'Error occured' });
+          });
+      });
+    }
+  },
 
-   async GetAllPosts(req, res) {
-     try {
-      const posts = await Post.find({})
-      .populate('user')
-      .sort({ created: -1 });
 
-      return res.status(HttpStatus.OK).json({message: 'All posts', posts });
-     } catch(err) {
-      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({message: 'Error occured' });
-     }
-   },
+  //  async GetAllPosts(req, res) {
+  //    try {
+
+  //     const today = moment().startOf('day');
+  //     const tomorrow = moment(today).add(1, 'days');
+
+  //     const posts = await Post.find({created: {$gte: today.toDate(), $lt: tomorrow.toDate()}}) //post filters 24hrs older: copy this to TOP posts below
+  //     .populate('user')
+  //     .sort({ created: -1 });
+
+  //     return res.status(HttpStatus.OK).json({message: 'All posts', posts });
+  //    } catch(err) {
+  //     return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({message: 'Error occured' });
+  //    }
+  //  },
 
    async AddLike(req, res) {
     const postId = req.body._id;
@@ -70,14 +131,32 @@ module.exports = {
    },
    async GetAllPosts(req, res) {
     try {
-     const posts = await Post.find({})
+      //post filters 24hrs older: copy this to TOP posts below
+      const today = moment().startOf('day');
+      const tomorrow = moment(today).add(5, 'days');
+  //posts
+     const posts = await Post.find({created: {$gte: today.toDate(), $lt: tomorrow.toDate()}})
      .populate('user')
      .sort({ created: -1 });
 
-     const top = await Post.find({totalLikes: {$gte: 2}})
+     const top = await Post.find({totalLikes: {$gte: 2}, created: {$gte: today.toDate(), $lt: tomorrow.toDate()}})
      .populate('user')
      .sort({ created: -1 });
-
+  //posts
+      const user = await User.findOne({_id: req.user._id});
+      if(!user.city && !user.country) {
+        request('http://geoip-db.com/json/', {json: true}, async (err, res, body) => {
+          await User.update({
+            _id: req.user._id
+          }, {
+            city: body.city,
+            country: body.country_name
+          }
+          );
+         }
+       );
+      }
+  
      return res.status(HttpStatus.OK).json({message: 'All posts', posts, top });
     } catch(err) {
      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({message: 'Error occured' });
